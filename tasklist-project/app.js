fb53d502c3e06b3463d2792f8f943df71e84ca80
Jennifer Okafor <jennakafor00@gmail.com>
2022-01-24 04:54:24 +0100

    practice

--- tasklist-project/app.js
+++ tasklist-project/app.js
@@ -1,3 +1,20 @@
-const taskTitle = (document.getElementById('task-title'));
+// const taskTitle = (document.getElementById('task-title'));
 
-document.querySelector('#task-title');
+// const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// items.forEach(function(item, index) {
+//     item.textContent = `${index}: Hello`;
+// }
+// )
+
+// console.log(items);
+
+let val;
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.colection-item:first-child');
+
+val = list;
+// get child nodes
+val = list.childNodes;
+
+console.log(val);
