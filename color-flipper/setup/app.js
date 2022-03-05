cc642c0fe84c3b1f3d6764ddbdcdeb29957fc57f
Jennifer Okafor <jennakafor00@gmail.com>
2022-03-05 11:55:29 +0100

    adding the solution

--- color-flipper/setup/app.js
+++ color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const colorText = document.querySelector('.color');
+const button = document.getElementById('btn');
+
+// function to change the background color and span text when the button is clicked
+
+button.addEventListener ('click', function () {
+    randomColor = random();
+    colorText.textContent = colors[randomColor];
+    document.body.style.backgroundColor = colors[randomColor];
+})
+
+// randomizing color
+
+random = () => {
+    return Math.floor(Math.random() * colors.length);
+}
--- color-flipper/setup/hex.js
+++ color-flipper/setup/hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const colorText = document.querySelector('.color');
+const button = document.getElementById('btn');
+
+// function to change the background color and span text when the button is clicked
+
+button.addEventListener ('click', function() {
+    let randomHex = "#";
+
+    for(let i = 0; i<6; i++) {
+        randomHex += hex[randomNumber()];
+    }
+
+    colorText.textContent = randomHex;
+    document.body.style.backgroundColor = randomHex;
+});
+
+// function to generate random numbers and/or letters 
+
+randomNumber = () => {
+    return Math.floor(Math.random() * hex.length);
+}
+
