52bffa1597ea9c5a377e582fe2e6c2929b2a844c
Jennifer Okafor <jennakafor00@gmail.com>
2022-03-14 17:09:52 +0100

    practice react page

---
+++ first-react/app.js
@@ -0,0 +1,35 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+                <small>© 2021 Jen development. All rights reserved.</small>
+            </footer>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
